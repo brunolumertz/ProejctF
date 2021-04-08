cd70d7d39b39fbe9d9f412f3edf2d867e2cb0e0f
brunolumertz <bruno14.lumertz@hotmail.com>
2021-04-08 14:52:52 -0300

    Update lista favoritos

--- app/src/main/java/lul/myapplication/dao/DataBase.kt
+++ app/src/main/java/lul/myapplication/dao/DataBase.kt
@@ -2,27 +2,27 @@ package lul.myapplication.dao
 
 import android.content.Context
 import androidx.room.Database
-import androidx.room.Entity
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import lul.myapplication.dao.FMigration.Companion.migration_1_2
-import lul.myapplication.dao.FMigration.Companion.migration_2_3
 import lul.myapplication.models.Filme
 
-@Database(entities = [Filme::class], version = 3, exportSchema = false)
-abstract class DataBase: RoomDatabase() {
+@Database(entities = [Filme::class], version = 2, exportSchema = false)
+abstract class DataBase : RoomDatabase() {
 
     abstract fun filmeDAO(): FilmesDAO
-    companion object{
+
+    companion object {
         @Volatile
         private var INSTANCE: DataBase? = null
-        fun getDatabase(context: Context): DataBase{
-            return INSTANCE ?: synchronized(this){
+        fun getDatabase(context: Context): DataBase {
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     DataBase::class.java,
-                    "filme-db")
-                    .addMigrations(migration_1_2, migration_2_3)
+                    "filme-db"
+                )
+                    .addMigrations(migration_1_2)
                     .build()
                 INSTANCE = instance
                 instance
--- app/src/main/java/lul/myapplication/dao/FMigration.kt
+++ app/src/main/java/lul/myapplication/dao/FMigration.kt
@@ -8,10 +8,10 @@ class FMigration {
         val migration_1_2 = object :Migration(1, 2){
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("CREATE TABLE 'Filme_Novo' " +
-                        "('id' INT PRIMARY KEY, " +
-                        "'tittle' TEXT NOT NULL, " +
-                        "'poster' TEXT NOT NULL, " +
-                        "'release' TEXT NOT NULL)"
+                        "('id' INTEGER PRIMARY KEY, " +
+                        "'tittle' TEXT, " +
+                        "'poster' TEXT, " +
+                        "'release' TEXT)"
                 )
                 database.execSQL("INSERT INTO Filme_Novo (" +
                         "id, tittle, poster, release) " +
@@ -20,11 +20,6 @@ class FMigration {
                 )
                 database.execSQL("DROP TABLE Filme")
                 database.execSQL("ALTER TABLE Filme_Novo RENAME TO Filme")
-            }
-        }
-        val migration_2_3 = object : Migration(2, 3){
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE Filme ADD COLUMN status INTEGER")
             }
         }
     }
