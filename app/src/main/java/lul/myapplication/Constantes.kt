8e0f40b16eaf05474eb0a68abfc1fd641ce1687b
brunolumertz <57117496+brunolumertz@users.noreply.github.com>
2021-04-19 13:29:12 -0300

    Acesso da camera
    Adição do botão para camera e suas funcionalidades como tirar foto selecionar para perfil e acesso da galeria

--- app/src/main/java/lul/myapplication/Constantes.kt
+++ app/src/main/java/lul/myapplication/Constantes.kt
@@ -12,3 +12,5 @@ const val ADD_FILME_MENSAGEM_JAVI = "Filme adicionado na lista Ja Vi"
 const val ADD_FILME_MENSAGEM_QUEROVER = "Filme adicionado na lista Quero Ver"
 const val CANCELAR = "CANCELAR"
 const val PESQUISA_DELAY = 900L
+const val TAKE_PICTURE = 1
+const val SELECT_PICTURE = 2
--- app/src/main/java/lul/myapplication/MainActivity.kt
+++ app/src/main/java/lul/myapplication/MainActivity.kt
@@ -1,12 +1,18 @@
 package lul.myapplication
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.view.View
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
@@ -15,11 +21,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.fragment_filme.*
+import kotlinx.android.synthetic.main.nav_menu_lateral.*
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toggle: ActionBarDrawerToggle
+    var currentPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,8 +60,8 @@ class MainActivity : AppCompatActivity() {
 
         nav_menu_view.setNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.perfil_menu_item -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT)
-                R.id.camera_menu_item -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT)
+                R.id.camera_menu_item -> abreCamera()
+                R.id.galeria_menu_item -> abreGaleria()
                 R.id.configura_menu_item -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT)
                 R.id.mapa_menu_item -> Toast.makeText(this, "Clicked item three", Toast.LENGTH_SHORT)
             }
@@ -65,6 +77,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TAKE_PICTURE && resultCode == Activity.RESULT_OK){
+            try {
+                val file = File(currentPath)
+                val uri = Uri.fromFile(file)
+                foto_do_perfil.setImageURI(uri)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+        if (requestCode == SELECT_PICTURE && resultCode == Activity.RESULT_OK){
+            try {
+                val uri = data!!.data
+                foto_do_perfil.setImageURI(uri)
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
             return true
@@ -72,5 +105,37 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    private fun abreCamera(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null){
+            var photoFile: File? = null
+            try {
+                photoFile = criaImagem()
+            }catch (e: IOException){
+                e.printStackTrace()
+            }
+            if (photoFile != null){
+                var photoUri = FileProvider.getUriForFile(this, "lul.myapplication.fileprovider", photoFile)
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, TAKE_PICTURE)
+            }
+        }
+    }
+
+    private fun abreGaleria(){
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(Intent.createChooser(intent, "Selecione a imagem"), SELECT_PICTURE)
+    }
+
+    private fun criaImagem(): File {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageName ="JPEG_"+timeStamp+"_"
+        var storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        var image = File.createTempFile(imageName, ".jpg", storageDir)
+        currentPath = image.absolutePath
+        return image
+    }
 
 }
